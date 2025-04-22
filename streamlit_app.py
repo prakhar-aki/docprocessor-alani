48b6e5e8f0f4bc7c60c96a61f32ee2ae21060142
prakhar-aki <prakhar.b@akigroup.com>
2025-04-22 14:08:23 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,14 +49,13 @@ openai_client = OpenAI(api_key=open_api_key)
 
 st.markdown("""
     <style>
-   @import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400;600&display=swap');
-
+   @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     * {
-        font-family: 'Epilogue', sans-serif !important;
+        font-family: 'Poppins', sans-serif !important;
     }
 
     html, body, [class^="css"], [class*="css"] {
-        font-family: 'Epilogue', sans-serif !important;
+        font-family: 'Poppins', sans-serif !important;
     }
     </style>
 """, unsafe_allow_html=True)
@@ -1172,10 +1171,9 @@ st.markdown("""
             
         }
         
-        @import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400;600&display=swap');
-
+        @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
         html, body {
-            font-family: 'Epilogue', sans-serif !important;
+            font-family: 'Poppins', sans-serif !important;
         }
             
         div[data-testid="stFileUploaderDropzoneInstructions"] small {
